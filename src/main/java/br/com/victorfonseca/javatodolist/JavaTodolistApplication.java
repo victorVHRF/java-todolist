27c7ca8c86ba0ace79609ad96aca0c98f5ffae51
Victor Hugo Rosa Fosneca <victor.vhrf@gmail.com>
2023-10-16 15:30:04 -0300

    feat: todolist feito em java com as aulas da rocketseat

---
+++ src/main/java/br/com/victorfonseca/javatodolist/JavaTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorfonseca.javatodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victorfonseca/javatodolist/JavaTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorfonseca.javatodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
